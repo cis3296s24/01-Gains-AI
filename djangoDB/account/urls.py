4db1b818db2f27b76563400cfda03e25571f68f9
tul24693 <tul24693@temple.edu>
2024-04-16 21:31:47 -0400

    Updated DB

--- djangoDB/account/urls.py
+++ djangoDB/account/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('logout/', views.logout_user, name = 'logout'),
     path('workout/', views.workout, name = 'workout'),
     path('music/', views.music, name = 'music'),
+    path('community/', views.community, name = 'community'),
+    path('checkin/', views.checkin, name = 'checkin'),
+    path('diet/', views.diet, name = 'diet'),
+    path('dietform/', views.dietform, name = 'dietform'),
 ]
