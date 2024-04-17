31543128ba7c20d792ae8a434903d5a309e4bb55
tul24693 <tul24693@temple.edu>
2024-04-16 21:36:52 -0400

    Created Model/Forms objects for Workouts

--- djangoDB/account/forms.py
+++ djangoDB/account/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.forms.widgets import PasswordInput, TextInput
+from .models import FitnessRecord
 
 class CreateUserForm(UserCreationForm):
     class Meta:
@@ -12,4 +13,9 @@ class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=TextInput())
     password = forms.CharField(widget=PasswordInput())
 
+
+class FitnessRecordForm(forms.ModelForm):
+    class Meta:
+        model = FitnessRecord
+        fields = '__all__'
 
--- djangoDB/account/models.py
+++ djangoDB/account/models.py
@@ -2,3 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class FitnessRecord(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    ]
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    duration = models.DurationField()
+    FITNESS_LEVEL_CHOICES = [
+        ('B', 'Beginner'),
+        ('I', 'Intermediate'),
+        ('A', 'Advanced'),
+    ]
+    fitness_level = models.CharField(max_length=1, choices=FITNESS_LEVEL_CHOICES)
+    workout_prompt = models.TextField()
+
+    def __str__(self):
+        return self.name
